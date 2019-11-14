a7ea35e3ba2e5c783bdf26473d5a8b40656d67c9
Florian Schulze <florian@contextflow.com>
2019-11-14 08:46:22 +0100

    add basic dcmt derivation

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+stdenv.mkDerivation rec {
+	version = "20170509";
+	name = "dcmtk-${version}";
+	src = fetchgit {
+		url = "git://git.dcmtk.org/dcmtk.git";
+		rev = "41853806778665673d36eeb196e5fa76e85c841a";
+		sha256 = "1lcwi02pryqs7yjyc6nz470bpr12n0ccsz1i0lwns4rv3sjb2g06";
+	};
+    buildInputs = [	zlib libtiff libpng libxml2 ];
+}
+
